99a3925439ddb7553a84288eb460e52f560f200b
Pavankumar0806 <114554186+Pavankumar0806@users.noreply.github.com>
2023-03-07 22:07:15 +0530

    Add files via upload

---
+++ java/com/webservice/restfullwebservices/RestfullwebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.webservice.restfullwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullwebservicesApplication.class, args);
+	}
+
+}
